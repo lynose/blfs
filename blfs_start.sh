c4a76296c03aa96eb900160921062bcee6484ca5
Fabian Theimert <lynose@Antares.localdomain>
2020-07-20 14:27:44 +0200

    a beginning

--- blfs_start.sh
+++ blfs_start.sh
@@ -5,9 +5,21 @@ ${log} `basename "$0"` " ======================================" blfs &&
 ${log} `basename "$0"`  "Started BLFS build" &&
 
 ${log} `basename "$0"` " ======================================" blfs &&
-./libs-common/libtasn1/01libtasn1.sh
+./libs-common/libtasn1/01libtasn1.sh &&
 ${log} `basename "$0"` " ======================================" blfs &&
 
+${log} `basename "$0"` " ======================================" blfs &&
+${log} `basename "$0"`  "Started Firefox section build" &&
+
+
+${log} `basename "$0"` " ======================================" blfs &&
+./devtools/autoconf-old/01-autoconf-2.16-old.sh &&
+
+${log} `basename "$0"` " ======================================" blfs &&
+./devtools/rustc/01-rustc-1.42.0.sh &&
+
+${log} `basename "$0"` " ======================================" blfs &&
+./devtools/cbindgen/01-cbindgen-0.14.2.sh &&
 
 
 ${log} `basename "$0"` " finished" blfs &&
