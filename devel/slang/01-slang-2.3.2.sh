257441170bda40c51b925136013da26640472f0c
Fabian Theimert <kosmonaut@theimert.net>
2020-11-29 08:20:02 +0100

    fix slang

--- devel/slang/01-slang-2.3.2.sh
+++ devel/slang/01-slang-2.3.2.sh
@@ -27,15 +27,18 @@ ${log} `basename "$0"` " configured" blfs_all &&
 make -j1 &&
 ${log} `basename "$0"` " built" blfs_all &&
 
-# make check &&
-# ${log} `basename "$0"` " unexpected check succeed" blfs_all
-# ${log} `basename "$0"` " expected check fail?" blfs_all &&
+if [ ${ENABLE_TEST} == true ]
+ then
+  make check &&
+  ${log} `basename "$0"` " unexpected check succeed" blfs_all
+  ${log} `basename "$0"` " expected check fail?" blfs_all
+fi
 
 as_root make install_doc_dir=/usr/share/doc/slang-2.3.2   \
      SLSH_DOC_DIR=/usr/share/doc/slang-2.3.2/slsh \
      install-all &&
 
-chmod -v 755 /usr/lib/libslang.so.2.3.2 \
+as_root chmod -v 755 /usr/lib/libslang.so.2.3.2 \
              /usr/lib/slang/v2/modules/*.so &&
 ${log} `basename "$0"` " installed" blfs_all &&
 ${log} `basename "$0"` " finished" blfs_all 
