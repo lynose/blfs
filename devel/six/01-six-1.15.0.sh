6e7abf62eeec91fa76e528d93c8e38f66674b07e
Fabian Theimert <kosmonaut@theimert.net>
2020-12-15 15:26:27 +0100

    add Gnu Debugger and Valgrind

---
+++ devel/six/01-six-1.15.0.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+${log} `basename "$0"` " started" blfs_all &&
+
+${log} `basename "$0"` " download" blfs_all &&
+if test -d /sources/six-1.15.0
+ then
+  rm -rf /sources/six-1.15.0
+fi
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+check_and_download https://files.pythonhosted.org/packages/source/s/six/six-1.15.0.tar.gz \
+        /sources
+
+
+md5sum -c ${SCRIPTPATH}/md5-six &&
+
+tar xf /sources/six-1.15.0.tar.gz -C /sources/ &&
+
+cd /sources/six-1.15.0 &&
+
+
+python2 setup.py build &&
+python3 setup.py build &&
+${log} `basename "$0"` " built" blfs_all &&
+
+python2 setup.py install --optimize=1 &&
+python3 setup.py install --optimize=1 &&
+${log} `basename "$0"` " installed" blfs_all &&
+${log} `basename "$0"` " finished" blfs_all 
