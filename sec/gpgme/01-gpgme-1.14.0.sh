ded869293ae051d8b306eba69f2bfdbae00bb9eb
Fabian Theimert <kosmonaut@theimert.net>
2021-01-05 13:27:47 +0100

    fix test gpgme

--- sec/gpgme/01-gpgme-1.14.0.sh
+++ sec/gpgme/01-gpgme-1.14.0.sh
@@ -25,8 +25,13 @@ ${log} `basename "$0"` " configured" blfs_all &&
 make &&
 ${log} `basename "$0"` " built" blfs_all &&
 
-#make -k check &&
-${log} `basename "$0"` " check succeed" blfs_all || &&
+if [ ${ENABLE_TEST} == true ]
+ then
+  make -k check &&
+  ${log} `basename "$0"` " check succeed" blfs_all ||
+  ${log} `basename "$0"` " expected check fail?" blfs_all
+fi
+
 
 as_root make install &&
 ${log} `basename "$0"` " installed" blfs_all &&
