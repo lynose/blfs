d2fa1cd37d2cd66fe5cdd36eed7d96e4f74b2bc1
Fabian Theimert <kosmonaut@theimert.net>
2021-01-15 11:51:30 +0100

    add new version of meson

---
+++ devel/meson/01-meson-5.56.0.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+${log} `basename "$0"` " started" blfs_all &&
+
+${log} `basename "$0"` " download" blfs_all &&
+if test -d /sources/meson-0.56.0
+ then
+  rm -rf /sources/meson-0.56.0
+fi
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+check_and_download https://github.com/mesonbuild/meson/releases/download/0.56.0/meson-0.56.0.tar.gz \
+        /sources &&
+
+check_and_download http://www.linuxfromscratch.org/patches/lfs/development/meson-0.56.0-upstream_fix-1.patch \
+        /sources &&
+
+        
+        
+md5sum -c ${SCRIPTPATH}/md5-meson &&
+
+tar xf /sources/meson-0.56.0.tar.gz -C /sources/ &&
+
+cd /sources/meson-0.56.0 &&
+
+patch -Np1 -i ../meson-0.56.0-upstream_fix-1.patch &&
+
+${log} `basename "$0"` " configured" blfs_all &&
+
+python3 setup.py build &&
+${log} `basename "$0"` " built" blfs_all &&
+
+python3 setup.py install --root=dest &&
+as_root cp -rv dest/* / &&
+${log} `basename "$0"` " installed" blfs_all &&
+${log} `basename "$0"` " finished" blfs_all 
