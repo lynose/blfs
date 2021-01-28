1a6606fef090c097ef04f4f2d45446070afea9a9
Fabian Theimert <kosmonaut@theimert.net>
2021-01-28 10:09:42 +0100

    reset to python-3.8.4

--- devel/python3/01-python-3.8.5.sh
+++ devel/python3/01-python-3.8.5.sh
@@ -2,22 +2,22 @@
 ${log} `basename "$0"` " started" blfs_all &&
 
 ${log} `basename "$0"` " download" blfs_all &&
-if test -d /sources/Python-3.9.1
+if test -d /sources/Python-3.8.5
  then
-  rm -rf /sources/Python-3.9.1
+  rm -rf /sources/Python-3.8.5
 fi
 
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
-check_and_download https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tar.xz \
+check_and_download https://www.python.org/ftp/python/3.8.5/Python-3.8.5.tar.xz \
     /sources &&
 
 md5sum -c ${SCRIPTPATH}/md5-python3 &&
 
-tar xf /sources/Python-3.9.1.tar.xz -C /sources/ &&
+tar xf /sources/Python-3.8.5.tar.xz -C /sources/ &&
 
-cd /sources/Python-3.9.1 &&
+cd /sources/Python-3.8.5 &&
 
 CXX="/usr/bin/g++"              \
 ./configure --prefix=/usr       \
@@ -25,6 +25,7 @@ CXX="/usr/bin/g++"              \
             --with-system-expat \
             --with-system-ffi   \
             --enable-optimization \
+            --with-lto          \
             --with-ensurepip=yes &&
 ${log} `basename "$0"` " configured" blfs_all &&
 
@@ -36,7 +37,7 @@ as_root make install &&
 
 as_root chmod -v 755 /usr/lib/libpython3.8.so &&
 as_root chmod -v 755 /usr/lib/libpython3.so &&
-as_root ln -svfn python-3.9.1 /usr/share/doc/python-3 &&
-as_root ln -svf /usr/bin/python3 python
+as_root ln -svfn python-3.8.5 /usr/share/doc/python-3 &&
+
 ${log} `basename "$0"` " installed" blfs_all &&
 ${log} `basename "$0"` " finished" blfs_all 
