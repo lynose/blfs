6c69ec9f7545f5f6046ae5be4ee7dcefd85b5e09
Fabian Theimert <dev@theimert.net>
2021-02-17 07:40:57 +0100

    add fltk

---
+++ X/fltk/01-fltk-1.3.5.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+${log} `basename "$0"` " started" blfs_all &&
+
+${log} `basename "$0"` " download" blfs_all &&
+if test -d /sources/fltk-1.3.5
+ then
+  rm -rf /sources/fltk-1.3.5
+fi
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+check_and_download http://fltk.org/pub/fltk/1.3.5/fltk-1.3.5-source.tar.gz \
+        /sources &&
+
+
+md5sum -c ${SCRIPTPATH}/md5-fltk &&
+
+tar xf /sources/fltk-1.3.5-source.tar.gz -C /sources/ &&
+
+cd /sources/fltk-1.3.5 &&
+
+sed -i -e '/cat./d' documentation/Makefile       &&
+
+./configure --prefix=/usr    \
+            --enable-shared  &&
+${log} `basename "$0"` " configured" blfs_all &&
+
+make &&
+make -C documentation html &&
+
+${log} `basename "$0"` " built" blfs_all &&
+
+if [ ${ENABLE_TEST} == true ]
+ then
+  test/unittests &&
+  ${log} `basename "$0"` " check succeed" blfs_all ||
+  ${log} `basename "$0"` " expected check fail?" blfs_all
+fi
+
+as_root make docdir=/usr/share/doc/fltk-1.3.5 install &&
+as_root make -C test          docdir=/usr/share/doc/fltk-1.3.5 install-linux &&
+as_root make -C documentation docdir=/usr/share/doc/fltk-1.3.5 install-linux &&
+${log} `basename "$0"` " installed" blfs_all &&
+${log} `basename "$0"` " finished" blfs_all 
